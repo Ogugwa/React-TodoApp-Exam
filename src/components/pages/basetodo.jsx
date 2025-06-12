dfee8674275112af72ac8ec6c1448d0d6158ec0f
Deborah Okolo <okoloogugwa@gmail.com>
2025-06-12 18:42:47 +0100

    The form to add new todo has been created next phase is to fetch the todo and display them on the page

--- src/components/pages/basetodo.jsx
+++ src/components/pages/basetodo.jsx
@@ -1,4 +1,5 @@
 // import { useQuery } from "@tanstack/react-query";
+// import { useForm, SubmitHandler } from "react-hook-form";
 
 // const { isPending, isError, data, error } = useQuery({
 //   queryKey: ["todos"],
@@ -6,6 +7,9 @@
 // });
 
 function BaseTodo() {
+  // const { register, handleSubmit } = useForm();
+  // const onSubmit = (data) => console.log(data);
+
   return (
     <div className="flex flex-col justify-center items-center m-4 p-4 font-medium">
       <div className="flex flex-col justify-center items-center font-medium p-2 gap-4">
@@ -14,6 +18,38 @@ function BaseTodo() {
           A modern task management app
         </p>
       </div>
+      {/* The form for adding new task */}
+      <section className=" w-full justify-center mt-2 items-center border-2 border-red-500">
+        <form className="flex flex-col font-medium shadow-md p-4">
+          <h2 className="text-xl mb-2">Add New Todo</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+            <div>
+              <label className="block text-md mb-2">Title</label>
+              <input
+                type="text"
+                placeholder="Add title....."
+                className="shadow-md border rounded w-full p-2"
+              />
+            </div>
+            <div>
+              <label className="block text-md mb-2">Description</label>
+              <input
+                type="text"
+                placeholder="Add description....."
+                className="shadow-md border rounded w-full p-2"
+              />
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="border rounded-md bg-blue-700 hover:bg-blue-500 p-2 px-4"
+              >
+                Add
+              </button>
+            </div>
+          </div>
+        </form>
+      </section>
       {/* This is the todo div container */}
       <section className="flex  justify-center items-center shadow-md">
         <div className="grid grid-cols-3 grid-rows-3"></div>
